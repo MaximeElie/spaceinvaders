564636f716827bb3405ea9df5b64ef95cde2c7a0
Maxime ELIE <maxurf@maxurf-PC>
2017-05-17 14:00:51 +0200

    positionner un nouveau vaisseau

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,5 +1,7 @@
 package fr.unilim.iut.spaceinvaders;
-  
+
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
 public class SpaceInvaders {
 	
 	private static final char MARQUE_FIN_LIGNE = '\n';
@@ -15,10 +17,14 @@ public class SpaceInvaders {
 	}
 	
 	public void positionnerUnNouveauVaisseau(int x, int y) {
-		if (x >= longueur)
-			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu");
+		if (  !estDansEspaceJeu(x, y) )
+			throw new HorsEspaceJeuException("La position du vaisseau est en dehors de l'espace jeu");
 		
 		this.vaisseau = new Vaisseau(x,y);
+	}
+
+	private boolean estDansEspaceJeu(int x, int y) {
+		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 	}
 	
 	@Override
